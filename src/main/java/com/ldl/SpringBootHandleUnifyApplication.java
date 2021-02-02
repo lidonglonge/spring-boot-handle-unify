cc9ae55b5dbda7f9fd1a893148bf53480ea57dec
李东龙 <lidonglonge@163.com>
2021-02-02 09:18:46 +0800

    初始化项目;spring boot 版本2.3.7.RELEASE

---
+++ src/main/java/com/ldl/SpringBootHandleUnifyApplication.java
@@ -0,0 +1,13 @@
+package com.ldl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHandleUnifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHandleUnifyApplication.class, args);
+    }
+
+}

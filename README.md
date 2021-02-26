245f285b87439c37a4762c91b519b2c93f6dc310
李东龙 <lidonglonge@163.com>
2021-02-26 11:15:37 +0800

    去掉多余信息

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # 工程简介
-用于快速搭建简单项目
+## 用于快速搭建简单项目
 
 spring boot
+
 Mybatis-plus
+
 knife4j接口管理
+
 日志管理
+
 代码生成
 
 
--- src/main/java/com/ldl/util/generator/MySQL8CodeGenerator.java
+++ src/main/java/com/ldl/util/generator/MySQL8CodeGenerator.java
@@ -13,14 +13,14 @@ public class MySQL8CodeGenerator {
 
     public static void main(String[] args) {
         DbType dbType = DbType.MYSQL;
-        String dbUrl = "jdbc:mysql://127.0.0.1:3306/saomaji?serverTimezone=UTC";
+        String dbUrl = "jdbc:mysql://127.0.0.1:3306/test?serverTimezone=UTC";
         String username = "root";
         String password = "root";
         String driver = "com.mysql.cj.jdbc.Driver";
         // 表前缀，生成的实体类，不含前缀
         String [] tablePrefixes = {};
         // 表名，为空，生成所有的表
-        String [] tableNames = {"test"};
+        String [] tableNames = {};
         // 字段前缀
         String [] fieldPrefixes = {};
         // 基础包名
